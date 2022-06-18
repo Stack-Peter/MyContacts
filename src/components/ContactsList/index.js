f1e06f4d8d436d5bd0fe41c8d8755a13c7fd1b2f
Pedro Marcos Ferreira <stack.peter21@gmail.com>
2022-06-18 18:33:16 -0300

    first commit

---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,14 @@
+import { Container, Header } from './styles'
+
+const ContactsList = () => {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href='/'>Novo Contato</a>
+      </Header>
+    </Container>
+  )
+}
+
+export default ContactsList
