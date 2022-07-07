f10fcee6cd20b19944a9b681e5bff80b89420612
stack.peter21 <stackpeter21@gmail.com>
2022-07-06 23:55:38 -0300

    melhorias UX input e-mail

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -45,7 +45,7 @@ export default function ContactForm({ buttonLabel }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} noValidate>
       <FromGroup error={getErrorMessageByFieldName("name")}>
         <Input
           error={getErrorMessageByFieldName("name")}
@@ -57,6 +57,7 @@ export default function ContactForm({ buttonLabel }) {
 
       <FromGroup error={getErrorMessageByFieldName("email")}>
         <Input
+          type="email"
           error={getErrorMessageByFieldName("email")}
           placeholder="E-mail"
           value={email}
